d904a9d1d4f85abc0a1df6a60a42760600614f5f
Nguyen <tranchauthanhnguyen@gmail.com>
2022-03-28 23:20:21 +0700

    uploadB12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Header from './components/Header/Header';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+          <Header/>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/redux/configStore.js
@@ -0,0 +1,8 @@
+import { combineReducers,createStore } from "redux";
+
+const rootReducer = combineReducers({
+    //Khai báo state (reducer)
+
+});
+
+export const store =  createStore(rootReducer)
